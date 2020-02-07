dd9700826dedc089de185295f61ae1a6262d79ef
2Young4Code <40252424+2Young4Code@users.noreply.github.com>
2020-02-07 11:03:59 +0200

    added hexgame

---
+++ HexGame/parameters.js
@@ -0,0 +1,12 @@
+var game = game || {};
+game.rows = 7;
+game.pink = game.pink || {};
+game.pink.cells = [ '23', '32', '33', '42', '43', '44'];
+game.orange = game.orange || {};
+game.orange.cells = [ '26', '27', '36','25','35','46'];
+game.pink.stars = ['35','36','47'];
+game.pink.trangles = ['25','26','27'];
+game.pink.reverseTriangles = [];
+game.orange.stars = ['32','33','22'];
+game.orange.trangles = [];
+game.orange.reverseTriangles = ['42','43','44'];
